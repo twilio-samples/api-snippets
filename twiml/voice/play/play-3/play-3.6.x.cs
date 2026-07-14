9185ee1af062256c45c28863a2bbe363188cda7e
rchinn <rchinn@twilio.com>
2026-07-14 11:36:22 -0700

    Fix Play digits pause sample

--- twiml/voice/play/play-3/play-3.6.x.cs
+++ twiml/voice/play/play-3/play-3.6.x.cs
@@ -8,7 +8,7 @@ class Example
     static void Main()
     {
         var response = new VoiceResponse();
-        response.Play(digits: "wwww3");
+        response.Play(digits: "www3");
 
         Console.WriteLine(response.ToString());
     }
